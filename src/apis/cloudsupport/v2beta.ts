60c47d9354e125f0190bfe87baacba1c646729f0
Yoshi Automation <yoshi-automation@google.com>
2022-02-26 01:31:59 +0000

    fix(cloudsupport): update the API
    #### cloudsupport:v2beta
    
    The following keys were changed:
    - resources.cases.methods.list.parameters.filter.description

--- src/apis/cloudsupport/v2beta.ts
+++ src/apis/cloudsupport/v2beta.ts
@@ -1702,7 +1702,7 @@ export namespace cloudsupport_v2beta {
      *
      *   // Do the magic
      *   const res = await cloudsupport.cases.list({
-     *     // An expression written in the Cloud filter language. If non-empty, then only cases whose fields match the filter are returned. If empty, then no messages are filtered out. Filter strings can use the following fields: - status (Accepted values: OPEN or CLOSED) - severity (Accepted values: S0, S1, S2, S3, or S4) - creator.email with the operators equals (=) and AND. Additionally, a global restriction (with no operator) can be used to search across displayName, description, and comments (e.g. "my search").
+     *     // An expression written in the Cloud filter language. If non-empty, then only cases whose fields match the filter are returned. If empty, then no messages are filtered out. Filter strings can use the following fields: - state (Accepted values: OPEN or CLOSED) - severity (Accepted values: S0, S1, S2, S3, or S4) - creator.email with the operators equals (=) and AND. Additionally, a global restriction (with no operator) can be used to search across displayName, description, and comments (e.g. "my search").
      *     filter: 'placeholder-value',
      *     // The maximum number of cases fetched with each request. Defaults to 10.
      *     pageSize: 'placeholder-value',
@@ -2152,7 +2152,7 @@ export namespace cloudsupport_v2beta {
   }
   export interface Params$Resource$Cases$List extends StandardParameters {
     /**
-     * An expression written in the Cloud filter language. If non-empty, then only cases whose fields match the filter are returned. If empty, then no messages are filtered out. Filter strings can use the following fields: - status (Accepted values: OPEN or CLOSED) - severity (Accepted values: S0, S1, S2, S3, or S4) - creator.email with the operators equals (=) and AND. Additionally, a global restriction (with no operator) can be used to search across displayName, description, and comments (e.g. "my search").
+     * An expression written in the Cloud filter language. If non-empty, then only cases whose fields match the filter are returned. If empty, then no messages are filtered out. Filter strings can use the following fields: - state (Accepted values: OPEN or CLOSED) - severity (Accepted values: S0, S1, S2, S3, or S4) - creator.email with the operators equals (=) and AND. Additionally, a global restriction (with no operator) can be used to search across displayName, description, and comments (e.g. "my search").
      */
     filter?: string;
     /**
