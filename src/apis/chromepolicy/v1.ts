f7b2a5856dbf432e4ec36432d8ebf12bd1a114b8
Yoshi Automation <yoshi-automation@google.com>
2022-02-26 01:31:58 +0000

    feat(chromepolicy): update the API
    #### chromepolicy:v1
    
    The following keys were added:
    - schemas.GoogleChromePolicyV1ResolvedPolicy.properties.addedSourceKey.$ref
    - schemas.GoogleChromePolicyV1ResolvedPolicy.properties.addedSourceKey.description
    - schemas.GoogleChromePolicyV1ResolvedPolicy.properties.addedSourceKey.readOnly

--- src/apis/chromepolicy/v1.ts
+++ src/apis/chromepolicy/v1.ts
@@ -371,6 +371,10 @@ export namespace chromepolicy_v1 {
    */
   export interface Schema$GoogleChromePolicyV1ResolvedPolicy {
     /**
+     * Output only. The added source key establishes at which level an entity was explicitly added for management. This is useful for certain type of policies that are only applied if they are explicitly added for management. For example: apps and networks. An entity can only be deleted from management in an Organizational Unit that it was explicitly added to. If this is not present it means that the policy is managed without the need to explicitly add an entity, for example: standard user or device policies.
+     */
+    addedSourceKey?: Schema$GoogleChromePolicyV1PolicyTargetKey;
+    /**
      * Output only. The source resource from which this policy value is obtained. May be the same as `targetKey` if the policy is directly modified on the target, otherwise it would be another resource from which the policy gets its value (if applicable). If not present, the source is the default value for the customer.
      */
     sourceKey?: Schema$GoogleChromePolicyV1PolicyTargetKey;
